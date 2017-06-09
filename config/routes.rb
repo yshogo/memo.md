1ca11cb9d4574177841357918e283c7d95a8f969
shogo.yamada <superman199323@gmail.com>
2017-06-09 23:32:12 +0900

    homeページおよび画面レイアウトの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/about'
+  get '/home', to:'static_pages#home'
+  get '/about',to:'static_pages#about'
+  get '/singup',to:'users#new'
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
   test 'shoud get about' do
-    get static_pages_about_url
+    get about_path
     assert_response :success
   end
 
